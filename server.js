8a5fedbe794889349f8e09a715972525373c1e05
nforcode <103197197+nforcode@users.noreply.github.com>
2025-05-26 18:25:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api', router);
+server.use(router);
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running');
 });
